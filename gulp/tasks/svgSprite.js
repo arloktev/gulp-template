d50a458831bdff888714668450ecab4cf7f19cde
Loktev Alexey <arloktev@mail.ru>
2021-11-28 08:25:00 +0300

    feat(svgSprite): скорректировал сборку svgSprite

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -1,17 +1,14 @@
 const { src, dest } = require('gulp');
-const gulpSvgSprite = require('gulp-svg-sprite');
+const svgstore = require('gulp-svgstore');
 const browserSync = require('browser-sync');
 const plumber = require('gulp-plumber');
 
-const paths = require('../paths');
+const { paths } = require('../config');
 
 const svgSprite = () => {
   return src(paths.sprites.src)
     .pipe(plumber())
-    .pipe(gulpSvgSprite({
-      mode: 'symbols',
-      preview: false
-    }))
+    .pipe(svgstore())
     .pipe(dest(paths.sprites.dist))
     .pipe(browserSync.stream());
 }
